42cfb28bfb660838692fbac9305501ab0cfdcf5b
Tomasz Rumpca <tomasz.rumpca@misys.com>
2018-11-04 23:33:15 +0100

    resolver service

---
+++ src/app/resolver.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {SeaportsTableItem} from './seaports-table/seaports-table-datasource';
+import {AlgorithmInput} from './_models/algorithmInput';
+import {Coordinates} from './_models/coordinates';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResolverService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  resolve(origin: SeaportsTableItem, destination: SeaportsTableItem, tripDate) {
+    const input = new AlgorithmInput(new Coordinates(origin.latitude, origin.longitude),
+      new Coordinates(destination.latitude, destination.longitude),
+      tripDate);
+
+    const data = localStorage.getItem('currentUser');
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    console.log('logged in user', data);
+    return this.http.post('http://localhost:8080/api/solve', input, {
+      headers: headers
+    });
+  }
+}
