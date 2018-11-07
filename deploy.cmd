6c25fae486d36427fc605a21d2defae6c7ddbb3b
Tomasz Rumpca <tomasz.rumpca@misys.com>
2018-11-07 22:13:13 +0100

    aligned output path, added web.config to angular.json

--- deploy.cmd
+++ deploy.cmd
@@ -91,21 +91,21 @@ goto :EOF
 echo Handling node.js deployment.
 
 :: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
+:: IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+::   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+::   IF !ERRORLEVEL! NEQ 0 goto error
+:: )
 
 :: 2. Select node version
 call :SelectNodeVersion
 
 :: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --production
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
+:: IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+::   pushd "%DEPLOYMENT_TARGET%"
+::   call :ExecuteCmd !NPM_CMD! install --production
+::   IF !ERRORLEVEL! NEQ 0 goto error
+::   popd
+:: )
 
 :: 4. Building the Angular App
 IF EXIST "%DEPLOYMENT_TARGET%\angular.json" (
